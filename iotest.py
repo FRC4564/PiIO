bbc2530c0162c340ab8024d2701c1b4146131093
Steven Jacobs <stevejacobs@iname.com>
2014-07-26 09:36:15 -0400

    Sample code

--- iotest.py
+++ iotest.py
@@ -2,28 +2,44 @@ import io
 import time
 
 try:
-    winch = io.Spike(1)
-    upperLimit = io.Switch(6)
-    lowerLimit = io.Switch(5)
+    sp1 = io.Spike(1)
+    sp2 = io.Spike(2)
+    sp3 = io.Spike(3)
+    sp4 = io.Spike(4)
+    sw1 = io.Switch(1)
+    sw2 = io.Switch(2)
+    sw3 = io.Switch(3)
+    sw4 = io.Switch(4)
+    sw5 = io.Switch(5)
+    sw6 = io.Switch(6)
 
-    winch.stop()
-    time.sleep(1)
-    winch.fwd()
-    time.sleep(1)
-    winch.rev()
-    time.sleep(1)
+    cycle = 0  #0=stop, 1=fwd, 2=rev
+    
+    while sw6.open():
+	if cycle == 0:
+	    sp1.stop()
+	    sp2.stop()
+            sp3.stop()
+            sp4.stop()
+            cycle = 1
+        elif cycle == 1:
+            sp1.fwd()
+            sp2.fwd()
+            sp3.fwd()
+            sp4.fwd()
+            cycle = 2
+        else:
+            sp1.rev()
+            sp2.rev()
+            sp3.rev()
+            sp4.rev()
+            cycle = 0
+        
+        print sw1.closed(),sw2.closed(),sw3.closed(),sw4.closed(),sw5.closed(),sw6.closed()
+        
+        time.sleep(.5)
+    
 
-    # Run winch to upper limit
-    while upperLimit.open():
-        winch.fwd()
-    winch.stop()
-    time.sleep(1)
-    
-    # Run winch to lower limit
-    while lowerLimit.open():
-        winch.rev()
-    winch.stop()
-    
 except:
     io.close()
     raise
